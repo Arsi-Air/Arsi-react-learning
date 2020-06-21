6863ebb96560e691e41b128d7e25197c29665d8b
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-21 19:47:55 +0400

    update

--- src/Room.js
+++ src/Room.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
-import './Room.css';
+import "./Room.css";
 
 const Room = () => {
     let [isLit, setLit] = useState(true);
     let [isClicks, setClicks] = useState(0);
+    let [isTemp, setTemp] = useState(0);
+    const brightness = isLit ? "lit" : "dark";
     let togLight = () => {
         setLit(!isLit);
         setClicks(++isClicks);
     }
 
     return(
-        <div className="room">This room is {isLit ? 'lit' : 'dark'}
+        <div className={`room ${brightness}`}>This room is {isLit ? "lit" : "dark"}
         <br />
-        Number of times light has been toggled: {isClicks}
+        Number of times toggled light has been pressed: {isClicks}
         <br />
         <button onClick = {togLight} > Toggle light</button>
+        <br />
+        <button onClick = {() => {setLit(true)}}>Turn On</button>
+        <br />
+        <button onClick = {() => {setLit(false)}}>Turn Off</button>
+        <br />
+        The temperature is {isTemp}
+        <br />
+        <button onClick = {() => setTemp(isTemp + 1)}>+</button>
+        <button onClick = {() => setTemp(isTemp - 1)}>-</button>
         </div>
     );
 }
