75e7be126470af14d87c6c85c640af09b777185e
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-22 10:27:50 +0400

    A Dark Room

--- src/Room.js
+++ src/Room.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Room.css";
 
 const Room = () => {
-    let [isLit, setLit] = useState(true);
+    let [isLit, setLit] = useState(false);
     let [isClicks, setClicks] = useState(0);
     let [isTemp, setTemp] = useState(0);
     //const brightness = isLit ? "lit" : "dark"; refer to *1* below
@@ -11,10 +11,12 @@ const Room = () => {
         setClicks(++isClicks);
     }
 
-    return(//<div className={`room ${brightness}`}> *1* These can be used together
-        <div className={`room ${isLit ? "lit" : "dark"}`}>This room is {isLit ? "lit" : "dark"}
+    return(
+        <div className={`room ${isLit ? "lit" : "dark"}`}>
+        <h1><center>A {isLit ? "Lit" : "Dark"} Room</center></h1>
+        This room is {isLit ? "lit" : "dark"}
         <br />
-        Number of times toggled light has been pressed: {isClicks}
+        Number of times toggle light has been pressed: {isClicks}
         <br />
         <button onClick = {togLight} > Toggle light</button>
         <br />
